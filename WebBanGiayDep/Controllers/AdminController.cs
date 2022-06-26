1ebf1af203037039b0a591ccef72630022654108
caonguyen21 <106511965+caonguyen21@users.noreply.github.com>
2022-06-26 20:33:53 +0700

    lam admin

---
+++ WebBanGiayDep/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanGiayDep.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
