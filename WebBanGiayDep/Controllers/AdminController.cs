e51d74dfd7d6ad5ffabd097f78a012d8044806f3
Halcyon1902 <huynhgiahao1902@gmail.com>
2022-06-27 04:28:42 +0700

    admin login

--- WebBanGiayDep/Controllers/AdminController.cs
+++ WebBanGiayDep/Controllers/AdminController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanGiayDep.Models;
 
 namespace WebBanGiayDep.Controllers
 {
+    
     public class AdminController : Controller
     {
+        dbShopGiayDataContext data = new dbShopGiayDataContext();
         // GET: Admin
         public ActionResult Index()
         {
@@ -16,6 +19,34 @@ namespace WebBanGiayDep.Controllers
         [HttpGet]
         public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            
+            var tendn = collection["username"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                QUANLY ad = data.QUANLies.SingleOrDefault(n => n.TaiKhoanQL == tendn && n.MatKhau == matkhau);
+                if (ad != null)
+                {
+                    ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["TaiKhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
             return View();
         }
     }
